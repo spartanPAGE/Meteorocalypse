e8f3293836a1c78cb34fa561cecda62a18aa8774
spartanPAGE <spartanpageagain@gmail.com>
2015-08-08 14:53:33 +0200

    Added test for characters from resources/test/characters/

--- src/charactersloadertest.cpp
+++ src/charactersloadertest.cpp
@@ -2,8 +2,8 @@
 #include "charactersmanager.hpp"
 
 SCENARIO("Can load characters", "[CharactersLoader]"){
-    GIVEN("New instance of CharactersLoader"){
-        CharactersLoader loader;
+    GIVEN("New instance of CharactersLoader with paths set on resources->test->characters"){
+        CharactersLoader loader("resources/test/characters/", "../Meteorocalypse/resources/test/characters/");
         WHEN("Trying to load list of characters"){
             THEN("At the begining, path is unspecified"){
                 REQUIRE(loader.path_in_use() == CharactersLoader::PathInUse::UNSPECIFIED);
@@ -17,6 +17,18 @@ SCENARIO("Can load characters", "[CharactersLoader]"){
                     loader.load_list();
                     THEN("the loaded_list is not empty"){
                         REQUIRE(loader.has_loaded_list());
+                    }
+                    WHEN("loading characters"){
+                        auto characters = loader.load_characters();
+                        THEN("the characters size is eq to 2 (count of characters in test->characters)"){
+                            REQUIRE(characters.size() == 2);
+                        }
+                        WHEN("Checking first character (named Tomasz)"){
+                            auto &tomasz = characters.front();
+                            THEN("Tomasz should match the .tsinfo representation"){
+                                REQUIRE(tomasz.name == "Tomasz");
+                            }
+                        }
                     }
                 }
             }

e3d5101ebdedda504172eb234889734ae059aca9
spartanPAGE <spartanpageagain@gmail.com>
2015-08-08 13:48:20 +0200

    Added simplifing wrapper for defaultless TreeStructInfo::parser_type

--- include/tsiloader.hpp
+++ include/tsiloader.hpp
@@ -1,5 +1,53 @@
-#ifndef TSILOADER_HPP
-#define TSILOADER_HPP
+#pragma once
 
-#endif // TSILOADER_HPP
+#include "Parser/ParserHelper.hpp"
 
+#include "Parser/Collector.hpp"
+#include "Parser/CollectedMemory.hpp"
+#include "Parser/InterpretStrategiesResponsibilityChain.hpp"
+#include "Parser/Parser.hpp"
+#include "Util/StreamTools.hpp"
+
+#include "InterpretStrategies/All.hpp"
+#include "InterpretStrategies/TagsMapped.hpp"
+
+#include "Parser/StateMachine.hpp"
+
+#include "Default.hpp"
+#include "DefaultTree.hpp"
+
+#include <array>
+#include <stack>
+#include <sstream>
+#include <algorithm>
+#include <tuple>
+#include <functional>
+
+
+
+namespace TSILoader{
+    using namespace TreeStructInfo;
+    using TreeStructInfo::Default::String;
+
+    template<typename MemT>
+    using collector_template_type = Collector<MemT>;
+    template<typename T>
+    using stack_template_type = Default::Stack<T>;
+    template<typename T>
+    using deque_template_type = Default::Deque<T>;
+    using tree_type = Default::Tree;
+    using attribute_type = Default::Attribute;
+
+    using memory_type = CollectedMemory<stack_template_type, deque_template_type, tree_type, attribute_type>;
+    using collector_type = Collector<memory_type>;
+
+    using string_type = Default::String;
+    using chain_type = InterpretStrategiesResponsibilityChain<collector_type, string_type>;
+    using machine_type = StateMachine<chain_type>;
+    using parser_type = Parser<machine_type>;
+    using strategy_type = chain_type::line_handle_strategy_type;
+    using dict_type = Default::Map<StrategyTag, strategy_type>;
+
+    using path_type = String;
+    tree_type load_tree(const path_type &);
+}
--- src/tsiloader.cpp
+++ src/tsiloader.cpp
@@ -1,1 +1,11 @@
+#include "tsiloader.hpp"
+#include <fstream>
 
+namespace TSILoader{
+    tree_type load_tree(const path_type &path){
+        parser_type parser;
+        std::ifstream in(path);
+        if(!in.good()) throw std::runtime_error("Invalid path");
+        return parser.loadTreeFromTextStream(in);
+    }
+}

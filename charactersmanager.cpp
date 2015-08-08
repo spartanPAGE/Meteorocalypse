6c108b184584d997e9b23a10cc455c49d5858370
spartanPAGE <spartanpageagain@gmail.com>
2015-08-08 09:56:17 +0200

    Added workspace for CharactersLoader

--- charactersmanager.cpp
+++ charactersmanager.cpp
@@ -1,3 +1,10 @@
 #include "charactersmanager.hpp"
 
-CharactersManager::CharactersManager(){}
+CharactersLoader::CharactersLoader(const Path &main, const Path &alter):
+    main_path(main), alternative_path(alter){}
+
+CharactersLoader::CharactersLoader(Path &&main, Path &&alter):
+    main_path(main), alternative_path(alter){}
+
+CharactersManager::CharactersManager(CharactersLoader &&loader):
+    characters_loader(loader){}
--- charactersmanager.hpp
+++ charactersmanager.hpp
@@ -1,10 +1,25 @@
 #pragma once
 #include "character.hpp"
 #include <map>
+#include <string>
+
+class CharactersLoader{
+public:
+    using Path = std::string;
+public:
+    CharactersLoader(const Path &main_path, const Path &alternative_path);
+    CharactersLoader(Path &&main_path        = "resources/characters/",
+                     Path &&alternative_path = "../Meteorocalypse/resources/characters/");
+private:
+    const Path main_path;
+    const Path alternative_path;
+    const Path characters_list = "list.txt";
+};
 
 class CharactersManager{
 public:
-    CharactersManager();
+    CharactersManager(CharactersLoader &&);
 private:
+    CharactersLoader characters_loader;
     std::map<Character::Name, Character> characters;
 };

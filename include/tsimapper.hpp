7c0f67f5e5cedb8bbf765b4726c1dca358f6bd78
spartanPAGE <spartanpageagain@gmail.com>
2015-08-08 12:07:05 +0200

    Added tsi mapper workspace

---
+++ include/tsimapper.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include "character.hpp"
+#include "DefaultTree.hpp"
+
+namespace TSIMapper{
+    Character to_character(const TreeStructInfo::Default::Tree &);
+}
---
+++ src/tsimapper.cpp
@@ -0,0 +1,9 @@
+#include "tsimapper.hpp"
+
+namespace TSIMapper{
+    Character to_character(TreeStructInfo::Default::Tree &tree){
+        Character character;
+        //Todo
+        return character;
+    }
+}

b12183b945292664d4abff2fbb948d0ddf1591b1
spartanPAGE <spartanpageagain@gmail.com>
2015-08-08 11:27:54 +0200

    ups, move, not copy

--- day.cpp
+++
@@ -1,7 +0,0 @@
-#include "day.hpp"
-
-Day::Day(Number num): id(num){}
-
-Day::Number Day::number() const{
-    return id;
-}
--- eventsmanager.cpp
+++
@@ -1,3 +0,0 @@
-#include "eventsmanager.hpp"
-
-EventsManager::EventsManager(){}

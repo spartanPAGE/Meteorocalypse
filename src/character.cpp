924cc45034a73bfaf26b5f110f2d010270e56aa8
spartanPAGE <spartanpageagain@gmail.com>
2015-08-24 12:20:26 +0200

    Extended runtime_error message with bad string value

--- src/character.cpp
+++ src/character.cpp
@@ -5,6 +5,6 @@ Character::Personality character_personality_from_string(const std::string &str)
     if(str == "FRIENDLY")
         return Character::Personality::FRIENDLY;
     if(str != "UNFRIENDLY")
-        throw std::runtime_error("Cannot convert string to character personality; Reason: bad string");
+        throw std::runtime_error("Cannot convert string to character personality; Reason: bad string;" + str);
     return Character::Personality::UNFRIENDLY;
 }

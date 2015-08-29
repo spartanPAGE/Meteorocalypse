50e7fef41000bfb1e067312e8a95c45e2b221385
Patryk Wertka <spartanpageagain@gmail.com>
2015-08-29 18:36:06 +0200

    Updated for itemproxy

--- src/items/itemsloadertest.cpp
+++ src/items/itemsloadertest.cpp
@@ -1,6 +1,9 @@
 #include "Catch.hpp"
 
 #include "items/itemsloader.hpp"
+#include "items/itemproxy.hpp"
+
+using namespace std;
 
 SCENARIO("Loading items", "[ItemsLoader]"){
     GIVEN("New instance of ItemsLoader with paths set on resources->test->items"){
@@ -11,18 +14,30 @@ SCENARIO("Loading items", "[ItemsLoader]"){
             GIVEN("Loaded list of items"){
                 auto items = loader.load(load_item);
                 WHEN("Validating size"){
-                    THEN("items.size() must be eq 2"){
-                        REQUIRE(items.size() == 2);
+                    THEN("items.size() must be eq 3"){
+                        REQUIRE(items.size() == 3);
                     }
                 }
 
                 WHEN("Validating items"){
                     THEN("First item is radio and the second is axe"){
-                        auto radio = items.front();
-                        REQUIRE(radio.name == "radio");
+                        ItemProxy radio(items.front());
+                        REQUIRE(radio.name() == "radio");
 
-                        auto axe = items.back();
-                        REQUIRE(axe.name == "axe");
+                        REQUIRE(radio.is_spoilable());
+                        REQUIRE_FALSE(radio.is_stackable());
+
+                        ItemProxy axe(*next(items.begin()));
+                        REQUIRE(axe.name() == "axe");
+
+                        REQUIRE(radio.is_spoilable());
+                        REQUIRE_FALSE(radio.is_stackable());
+
+                        ItemProxy rats_poison(*next(items.begin(), 2));
+                        REQUIRE(rats_poison.name() == "Rats poison");
+
+                        REQUIRE_FALSE(rats_poison.is_spoilable());
+                        REQUIRE(rats_poison.is_stackable());
                     }
                 }
             }
